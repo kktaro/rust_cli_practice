58d2821535cc8b9da7d61d1dbcec6e5630461780
kktaro <kk.nnkktr@gmail.com>
2025-04-15 19:38:58 +0900

    test: サンプルのインテグレーションテストを追加

---
+++ num1_hello/tests/cli.rs
@@ -0,0 +1,13 @@
+use std::process::Command;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok());
+}
