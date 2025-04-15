d9ce9fcf7022940efe96ac146d439ee135e7b462
kktaro <kk.nnkktr@gmail.com>
2025-04-15 21:12:11 +0900

    feat: rechoに渡せる引数を制限

--- num2_echor/src/main.rs
+++ num2_echor/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version)]
+#[command(about = "Rust echo")]
+struct Cli {
+    /// Input text
+    #[arg(required = true, num_args = 1..)]
+    text: Vec<String>,
+
+    #[arg(short, num_args = 0)]
+    /// Do not print newline
+    newline: bool,
+}
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let args = Cli::parse();
+    println!("{:#?}", args);
 }
