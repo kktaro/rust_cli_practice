dd208f83eb9aff078a436828060064adcb57c59f
kktaro <kk.nnkktr@gmail.com>
2025-05-08 21:19:53 +0900

    feat: `count`メソッドを追加

--- num5_wcr/src/lib.rs
+++ num5_wcr/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, fs::File, io::{self, BufRead, BufReader}};
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 use clap::{Args, Parser};
 
@@ -50,7 +54,13 @@ pub fn run(config: Config) -> MyResult<()> {
     for file_name in config.files {
         match open(&file_name) {
             Err(err) => eprintln!("{}: {}", file_name, err),
-            Ok(_) => println!("Opened! {}", file_name),
+            Ok(file) => match count(file) {
+                Err(err) => eprintln!("{}: {}", file_name, err),
+                Ok(file_info) => println!(
+                    "\t{}\t{}\t{} {}",
+                    file_info.num_lines, file_info.num_words, file_info.num_chars, file_name
+                ),
+            },
         }
     }
     Ok(())
@@ -61,6 +71,42 @@ fn open(file_name: &str) -> MyResult<Box<dyn BufRead>> {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(file_name)?))),
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    loop {
+        let mut buf = String::new();
+        let bytes = file.read_line(&mut buf)?;
+        if bytes == 0 {
+            // 読み取りバイト数が0の場合はEOF
+            break;
+        }
+
+        num_lines += 1;
+        num_words += buf.split_whitespace().count();
+        num_bytes += bytes;
+        num_chars += buf.chars().count();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
 }
 
 #[derive(Parser, Debug)]
@@ -97,6 +143,8 @@ struct CountType {
 
 #[cfg(test)]
 mod test {
+    use std::io::Cursor;
+
     use anyhow::{Ok, Result};
 
     use super::*;
@@ -105,6 +153,67 @@ mod test {
     fn args_verify() -> Result<()> {
         use clap::CommandFactory;
         Cli::command().debug_assert();
+        Ok(())
+    }
+
+    fn generate_info(text: &str) -> FileInfo {
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        info.unwrap()
+    }
+
+    #[test]
+    fn test_count() -> Result<()> {
+        let info = generate_info("I don't want the world. I just want your half.\r\n");
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_count_empty() -> Result<()> {
+        let info = generate_info("");
+        let expected = FileInfo {
+            num_lines: 0,
+            num_words: 0,
+            num_chars: 0,
+            num_bytes: 0,
+        };
+        assert_eq!(info, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_count_single_line() -> Result<()> {
+        let info = generate_info("aaa");
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 1,
+            num_chars: 3,
+            num_bytes: 3,
+        };
+        assert_eq!(info, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_count_fox() -> Result<()> {
+        let info = generate_info(
+            r"The  quick brown fox	jumps over   the lazy dog.
+",
+        );
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 9,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info, expected);
         Ok(())
     }
 }
